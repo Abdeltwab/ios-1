038932055f2c6282012b87e74af41040f2120f41
Desislav Hristov <hristov.desislav@gmail.com>
2020-06-19 16:55:43 +0300

    ✨ Define the suggestions section

---
+++ Modules/Timer/Timer/TimeEntriesLog/ViewModels/TimeEntriesLogSectionViewModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum TimeEntriesLogSectionViewModel: Equatable {
+    static func == (lhs: TimeEntriesLogSectionViewModel, rhs: TimeEntriesLogSectionViewModel) -> Bool {
+        switch (lhs, rhs) {
+        case (let .day(lDayViewModel), let .day(rDayViewModel)):
+            return lDayViewModel == rDayViewModel
+        case (let .suggestions(lSuggestions), let .suggestions(rSuggestions)):
+            return lSuggestions == rSuggestions
+        default:
+            return false
+        }
+    }
+    
+    case day(DayViewModel)
+    case suggestions([SuggestionViewModel])
+}
