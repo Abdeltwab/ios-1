3fa98900db20585cb63faf0433c6b35def627507
Desislav Hristov <hristov.desislav@gmail.com>
2020-06-25 10:00:26 +0300

    ✨ Better project/client/task strings

--- Modules/Utils/Utils/UI/TextFormatting/ProjectClientTaskString.swift
+++ Modules/Utils/Utils/UI/TextFormatting/ProjectClientTaskString.swift
@@ -7,6 +7,22 @@ public func projectClientTaskString(projectName: String? = nil,
                                     taskName: String? = nil,
                                     clientName: String? = nil) -> NSAttributedString {
     let attributedString = NSMutableAttributedString()
+    
+    if let color = projectColor {
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(named: "projectCircle", in: Assets.bundle, compatibleWith: nil)!
+        let attachmentString = NSAttributedString(attachment: attachment)
+        let part = NSMutableAttributedString(attributedString: attachmentString)
+        part.addAttribute(.foregroundColor,
+                           value: UIColor(hex: color).projectColor,
+                           range: NSRange(location: 0, length: attachmentString.length))
+        attributedString.append(part)
+        
+        let spacer = NSTextAttachment()
+        spacer.bounds = CGRect(x: 0, y: 0, width: 5, height: 0)
+        attributedString.append(NSAttributedString(attachment: spacer))
+    }
+    
     if let project = projectName, let color = projectColor {
         var string = project
         if let task = taskName {
